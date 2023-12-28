b4f106ecf0acf87abf1989179d735c2385d3db44
Anhelina Lunova <luangieyu@gmail.com>
2023-12-28 22:10:20 +0200

    refactor loop and add comments

--- src/hearts_1.py
+++ src/hearts_1.py
@@ -38,13 +38,32 @@ def deal_hands(deck: Deck) -> Tuple[Deck, Deck, Deck, Deck]:
 
 def play() -> None:
     """Play a 4-player card game"""
-    deck = get_deck(shuffle=True)
-    names = "P1 P2 P3 P4".split()
-    hands: dict[str, Deck] = {n: h for n, h in zip(names, deal_hands(deck))}
+    deck = get_deck(shuffle=True)  # роздали колоду
+    names = "P1 P2 P3 P4".split()  # сформували імена гравців
+    hands: dict[str, Deck] = {
+        n: h for n, h in zip(names, deal_hands(deck))
+    }  # здали карти на руки
+    start_player = choose(
+        names
+    )  # get a name of player who will go first (випадково визначили імʼя першого гравця)
+    turn_order = player_order(
+        names, start=start_player
+    )  # сформували чергу від першого гравця і далі
 
-    for name, cards in hands.items():
-        card_str = " ".join(f"{s}{r}" for (s, r) in cards)
-        print(f"{name}: {card_str}")
+    while hands[start_player]:  # до моменту поки у першого гравця не закінчаться карти
+        for name in turn_order:
+            card = choose(
+                hands[name]
+            )  # поки в нього є карти ми випадково беремо карту для того, чий зараз хід
+            hands[name].remove(
+                card
+            )  # він взяв її, ми видаляємо карту щоб гравець знову її не взяв
+            print(f"{name}: {card[0] + card[1]:<3} ", end="")  # і після цього друкуємо
+        print()
+
+    # for name, cards in hands.items():
+    #     card_str = " ".join(f"{s}{r}" for (s, r) in cards)
+    #     print(f"{name}: {card_str}")
 
 
 if __name__ == "__main__":
