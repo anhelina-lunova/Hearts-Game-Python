8db8edca8a417980db1d42ffe8c8a43590103691
Anhelina Lunova <luangieyu@gmail.com>
2023-12-28 22:13:30 +0200

    remove extra types

--- src/hearts_1.py
+++ src/hearts_1.py
@@ -1,5 +1,5 @@
 import random
-from typing import List, Tuple, Sequence, Any, TypeVar, Optional
+from typing import List, Tuple, Sequence, TypeVar, Optional
 
 SUITS = "♠ ♥ ♦ ♣".split()
 RANKS = "2 3 4 5 6 7 8 9 10 J Q K A".split()
