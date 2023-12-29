05f50f793f24783c55015347f22688f145d4fbd0
Anhelina Lunova <luangieyu@gmail.com>
2023-12-29 02:50:03 +0200

    Update hearts_1.py

--- src/hearts_1.py
+++ src/hearts_1.py
@@ -1,66 +1,77 @@
 import random
-from typing import List, Tuple, Sequence, TypeVar, Optional
-
-SUITS = "♠ ♥ ♦ ♣".split()
-RANKS = "2 3 4 5 6 7 8 9 10 J Q K A".split()
-
-Card = Tuple[str, str]  # tuple[str, str]
-Deck = List[Card]  # list[tuple[str, str]
-
-Choosable = TypeVar("Choosable")
+import sys
 
 
-def get_deck(shuffle: bool = False) -> Deck:
-    """Get a new deck of 52 cards."""
-    deck = [(s, r) for r in RANKS for s in SUITS]
-    if shuffle:
-        random.shuffle(deck)
-    return deck
+class Card:
+    SUITS = "♠ ♥ ♦ ♣".split()
+    RANKS = "2 3 4 5 6 7 8 9 10 J Q K A".split()
+
+    def __init__(self, suit: str, rank: str) -> None:
+        self.suit = suit
+        self.rank = rank
+
+    def __repr__(self):
+        return f"{self.suit}{self.rank}"
 
 
-def choose(items: Sequence[Choosable]) -> Choosable:
-    """Choose and return random item."""
-    return random.choice(items)
+class Deck:
+    def __init__(self, cards):
+        self.cards = cards
+
+    @classmethod
+    def create(cls, shuffle=False):
+        """Create a new deck of 52 cards"""
+        cards = [Card(s, r) for r in Card.RANKS for s in Card.SUITS]
+        if shuffle:
+            random.shuffle(cards)
+        return cls(cards)
+
+    def deal(self, num_hands: int):
+        """Deal the cards in the deck into num_hands."""
+        cls = self.__class__
+        return tuple(cls(self.cards[i::num_hands]) for i in range(num_hands))
 
 
-def player_order(names: List[str], start: Optional[str] = None) -> List[str]:
-    """Rotate plyer order so that start goes first."""
-    if start is None:
-        start = choose(names)
-    start_index = names.index(start)
-    return names[start_index:] + names[:start_index]
+class Player:
+    def __init__(self, name: str, hand: Deck) -> None:
+        self.name = name
+        self.hand = hand
+
+    def play_card(self) -> Card:
+        """Play a card from the player's hand."""
+        card = random.choice(self.hand.cards)
+        self.hand.cards.remove(card)
+        print(f"{self.name}: {card!r:<3}", end="")
 
 
-def deal_hands(deck: Deck) -> Tuple[Deck, Deck, Deck, Deck]:
-    """Deal the cards in the deck into four hands"""
-    return deck[0::4], deck[1::4], deck[2::4], deck[3::4]
+class Game:
+    def __init__(self, *names: str) -> None:
+        deck = Deck.create(shuffle=True)
+        self.names = (
+            (list(names) + "P1 P2 P3 P4".split())[:4]
+            if names
+            else "P1 P2 P3 P4".split()[:4]
+        )
+        self.hands = {n: Player(n, h) for n, h in zip(self.names, deck.deal(4))}
 
+    def player_order(self, start=None):
+        if start is None:
+            start = random.choice(self.names)
+        start_idx = self.names.index(start)
+        return self.names[start_idx:] + self.names[:start_idx]
 
-def play() -> None:
-    """Play a 4-player card game"""
-    deck = get_deck(shuffle=True)  # роздали колоду
-    names = "P1 P2 P3 P4".split()  # сформували імена гравців
-    hands: dict[str, Deck] = {
-        n: h for n, h in zip(names, deal_hands(deck))
-    }  # здали карти на руки
-    start_player = choose(
-        names
-    )  # get a name of player who will go first (випадково визначили імʼя першого гравця)
-    turn_order = player_order(
-        names, start=start_player
-    )  # сформували чергу від першого гравця і далі
+    def play(self) -> None:
+        """Play a card game."""
+        start_player = random.choice(self.names)
+        turn_order = self.player_order(start=start_player)
 
-    while hands[start_player]:  # до моменту поки у першого гравця не закінчаться карти
-        for name in turn_order:
-            card = choose(
-                hands[name]
-            )  # поки в нього є карти ми випадково беремо карту для того, чий зараз хід
-            hands[name].remove(
-                card
-            )  # він взяв її, ми видаляємо карту щоб гравець знову її не взяв
-            print(f"{name}: {card[0] + card[1]:<3} ", end="")  # і після цього друкуємо
-        print()
+        while self.hands[start_player].hand.cards:
+            for name in turn_order:
+                self.hands[name].play_card()
+            print()
 
 
 if __name__ == "__main__":
-    play()
+    player_names = sys.argv[1:]
+    game = Game(*player_names)
+    game.play()
